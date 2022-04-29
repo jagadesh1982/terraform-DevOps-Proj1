981d2d1d11193aec1c0d3c6ce3e400ae04a3fec4
root <root@ip-172-31-91-12.ec2.internal>
2022-04-29 04:47:20 +0000

    Code Complete

---
+++ main.tf
@@ -0,0 +1,148 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "us-east-1"
+}
+
+resource "aws_sns_topic" "my_first_sns_topic" {
+  name = var.sns_name
+}
+
+resource "aws_sns_topic_policy" "my_sns_topic_policy" {
+  arn = aws_sns_topic.my_first_sns_topic.arn
+  policy = data.aws_iam_policy_document.my_custom_sns_policy_document.json
+}
+
+data "aws_iam_policy_document" "my_custom_sns_policy_document" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        var.account_id,
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_sns_topic.my_first_sns_topic.arn,
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+name   = "Spacelift_Test_Lambda_Function_Role"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+ 
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path         = "/"
+ description  = "AWS IAM Policy for managing aws lambda role"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+}
+ 
+data "archive_file" "zip_the_python_code" {
+type        = "zip"
+source_dir  = "${path.module}/lambda/"
+output_path = "${path.module}/lambda/hello-python.zip"
+}
+ 
+resource "aws_lambda_function" "terraform_lambda_func" {
+filename                       = "${path.module}/lambda/hello-python.zip"
+function_name                  = "Spacelift_Test_Lambda_Function"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "python3.9"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
+
+resource "aws_sns_topic_subscription" "topic_lambda" {
+  topic_arn = "${aws_sns_topic.my_first_sns_topic.arn}"
+  protocol  = "lambda"
+  endpoint  = "${aws_lambda_function.terraform_lambda_func.arn}"
+}
+
+resource "aws_dynamodb_table" "EKS_cluster_monitoring" {
+  name        = "${var.table_name}"
+  hash_key      = "cluster_name"
+  range_key     = "alert_name"
+  read_capacity  = 20
+  write_capacity = 20
+  
+  attribute {
+    name = "cluster_name"
+    type = "S"
+  }
+
+  attribute {
+    name = "alert_name"
+    type = "S"
+  }
+  
+   tags = {
+    environment       = "${var.environment}"
+  }
+
+}
+
+
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+access_key = "AKIAQC57DESUXG5T2XJS"
+secret_key = "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"
